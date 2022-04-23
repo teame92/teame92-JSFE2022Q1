8d185cb6611094284906ae19965c28304c2a03fa
Kirill Volosevich <keriaz@mail.ru>
2022-04-24 00:22:37 +0300

    feat: add burger menu

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,6 +1,12 @@
 const popup = document.querySelector('.popup')
 const btnPopup = document.querySelector('.btn-round__popup')
-const cards = document.querySelectorAll('.card a')
+const cards = document.querySelectorAll('.cards__slider')
+const burgerBtn = document.querySelector('.burger')
+const body = document.querySelector('.body')
+const logo = document.querySelector('.header__inner .logo')
+const header = document.querySelector('.header')
+const headerInner = document.querySelector('.header__inner').cloneNode(true)
+
 
 cards.forEach((v) => {
     v.addEventListener('click', (e) => {
@@ -28,7 +34,51 @@ popup.addEventListener('mouseover', (e) => {
 
 // burger
 
-const burgerBtn = document.querySelector('.burger')
-burgerBtn.addEventListener('click', (e) => {
-    burgerBtn.classList.toggle('burger__click')
+
+let modalOpen = false
+const div = document.createElement('div');
+div.classList.add('burger__section')
+div.insertAdjacentHTML('afterbegin', `<div class="burger__wrapper"></div>`);
+div.firstElementChild.append(headerInner);
+
+function burgerOpen() {
+    modalOpen = true
+    header.append(div)
+    burgerBtn.classList.add('burger__click')
+    body.classList.add('scroll')
+    logo.classList.add('logo__hidden')
+    document.querySelector('.burger__section').classList.add('burger__section-visible')
+    setTimeout(() => {
+            document.querySelector('.burger__wrapper').classList.add('burger__wrapper-open')
+        },
+        0
+    )
+}
+
+function burgerClose() {
+    modalOpen = false
+    burgerBtn.classList.remove('burger__click')
+    body.classList.remove('scroll')
+    logo.classList.remove('logo__hidden')
+    setTimeout(() => {
+            document.querySelector('.burger__section').classList.remove('burger__section-visible')
+        },
+        0
+    )
+
+    document.querySelector('.burger__wrapper').classList.remove('burger__wrapper-open')
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target.closest('.burger') && !modalOpen) {
+        burgerOpen()
+    } else if (!e.target.closest('.burger__wrapper') || e.target.closest('.navbar__link')) {
+        burgerClose()
+    }
+})
+
+window.matchMedia('(min-width: 768px)').addListener((e) => {
+    if (modalOpen) {
+        burgerClose()
+    }
 })

65dadaa8e6a4b5714453a9aef22d06e01e19cc22
Kirill Volosevich <keriaz@mail.ru>
2022-08-04 23:06:41 +0300

    feat: add render functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import './css/main.css';
 import { createBasicStructure } from './components/basicStructure';
+import { renderGaragePage, renderWinnersPage } from './renderPage';
 createBasicStructure();
+await renderGaragePage();
+await renderWinnersPage();
